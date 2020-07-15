9190a183944cf725974537597692bc9a26aac895
hahm-d <dhahm01@gmail.com>
2020-07-15 05:45:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -14,9 +14,11 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
+      #for each pirate's ships create a ship class 
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
+      #create a instance variable for ships to be called to pirates/show.erb
       @ships = Ship.all
       erb :'pirates/show'
     end
